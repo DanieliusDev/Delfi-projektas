c259072554780b2ac8ab9e03e0acf3d089ee341e
Dani <danikengland@gmail.com>
2019-10-18 10:32:01 +0300

    pasiduodu su testais, kazkas man nesigauna :(

--- src/test/java/parsetests/UnitTestForParse.java
+++ src/test/java/parsetests/UnitTestForParse.java
@@ -1,8 +1,19 @@
 package parsetests;
+import webzodziupaieska.data.NewsPapers;
+import webzodziupaieska.functionality.NewsReader;
 
 public class UnitTestForParse {
 
+    public boolean checkingFinalRez() {
+        NewsReader reader = new NewsReader();
 
+        if (reader.generateWithContent(NewsPapers.DELFI) != null) {
+
+            return true;
+        }
+
+        return false;
+    }
 
 
 }
